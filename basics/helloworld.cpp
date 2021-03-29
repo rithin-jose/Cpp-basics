451a3938ec1fbd518098a0df258bb2a5388602c9
unknown <rithinja@gmail.com>
2021-03-29 11:56:41 +0530

    Breakdown of code

--- basics/helloworld.cpp
+++ basics/helloworld.cpp
@@ -9,12 +9,25 @@ int main()
 }
 
 
+// Break down of the code
 
+/*
+#include<iostream>          Header: file used to get the input and output functionality
+
+using namespace std;        Namespace: make all the things under the std namespace available without having to prefix std:
+
+int main()                  Main function: this is where the execution of program begins
+{
+    cout<<"Hello World";
+    return 0;
+}
+*/
 
 /********************************************************
 Title: Hello World
 Author: CAC
 Date: 29th march 2021
+Description:
 This code was implemented on day 1 of 100 days of code
 and a part of refresher course for c++.
 *********************************************************/
