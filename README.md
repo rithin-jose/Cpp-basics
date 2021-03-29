393166d535b43727ea5f2e08d13bb79270d49e08
unknown <rithinja@gmail.com>
2021-03-29 11:02:17 +0530

    OLD Files Separated - New basics folder created - Boilerplate for 100 days of code created - Hello World added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Cpp-basics
+
+# C Plus Plus basics
+This repositor is intended to have the topics in cpp from basic to advances topics such as **STL** and implementation of **algorithms**.  For the best experience the following order can be followed:
+
+ 1. [Hello World](basics/helloworld.cpp)
---
+++ basics/helloworld.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    return 0;
+}
+
+
+
+
+/********************************************************
+Title: Hello World
+Author: CAC
+Date: 29th march 2021
+This code was implemented on day 1 of 100 days of code
+and a part of refresher course for c++.
+*********************************************************/
